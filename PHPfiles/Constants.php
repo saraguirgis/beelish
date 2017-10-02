2bd57daa8c8b4bc81a98b37b8b47515ba14deb88
Fady H. Wahba <fadyham@hotmail.com>
2017-10-01 20:58:46 -0700

    Added HTML helpers, and some constants

---
+++ PHPfiles/Constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * A class to hold Theme related constants.
+ */
+class ThemeConstants {
+    /** Table header background color. */
+    const TableHeaderBGColor = "#D9EDF7";
+    /** Table cell background color when there's no lunch available. */
+    const TableCellNoLunchBGColor = "#D3D3D3";
+    /** Table cell background color when lunch is available to order. */
+    const TableCellLunchAvailableBGColor = "#FFFFFF";
+    /** Table cell width in percentage. */
+    const TableCellWidthPercentage = "20%";
+    /** Table cell height in pixels. */
+    const TableCellHeightPixels = "100";
+}
+?>
