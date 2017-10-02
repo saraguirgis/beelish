93d69c53d11ab95f3c5e7bfded75c6b7f36a89c4
Fady H. Wahba <fadyham@hotmail.com>
2017-10-01 22:29:30 -0700

    Addressing CR comments

--- PHPfiles/Constants.php
+++ PHPfiles/Constants.php
@@ -5,8 +5,8 @@
 class ThemeConstants {
     /** Table header background color. */
     const TableHeaderBGColor = "#D9EDF7";
-    /** Table cell background color when there's no lunch available. */
-    const TableCellNoLunchBGColor = "#D3D3D3";
+    /** Table cell background color when there's no lunch available or ordering is expired. */
+    const TableCellNothingToOrderBGColor = "#D3D3D3";
     /** Table cell background color when lunch is available to order. */
     const TableCellLunchAvailableBGColor = "#FFFFFF";
     /** Table cell width in percentage. */
