e35930e8d58347270e651d59fa95419f8df54255
Fady H. Wahba <fadyham@hotmail.com>
2017-10-02 03:00:01 -0700

    Added oct-dec calendars

--- PHPfiles/ProductCalendar.php
+++ PHPfiles/ProductCalendar.php
@@ -6,45 +6,14 @@ class ProductCalendar {
     const NoLunchProductId = -1;
     const NoDetailsProductId = null;
 
-    function __construct() {
-        // Initialize product dates and IDs array
-        $this->products = array(
-            "09/11/2017" => ProductCalendar::NoLunchProductId,
-            "09/12/2017" => ProductCalendar::NoLunchProductId,
-            "09/13/2017" => ProductCalendar::NoLunchProductId,
-            "09/14/2017" => ProductCalendar::NoLunchProductId,
-            "09/15/2017" => ProductCalendar::NoLunchProductId,
-            
-            "09/18/2017" => ProductCalendar::NoLunchProductId,
-            "09/19/2017" => ProductCalendar::NoLunchProductId,
-            "09/20/2017" => ProductCalendar::NoLunchProductId,
-            "09/21/2017" => ProductCalendar::NoLunchProductId,
-            "09/22/2017" => ProductCalendar::NoLunchProductId,
-            
-            "09/25/2017" => 170,
-            "09/26/2017" => 167,
-            "09/27/2017" => 159,
-            "09/28/2017" => 389,
-            "09/29/2017" => 392,
-            
-            "10/02/2017" => 395,
-            "10/03/2017" => 401,
-            "10/04/2017" => 401,
-            "10/05/2017" => 437,
-            "10/06/2017" => 401,
-            
-            "10/09/2017" => 401,
-            "10/10/2017" => 401,
-            "10/11/2017" => ProductCalendar::NoDetailsProductId,
-            "10/12/2017" => ProductCalendar::NoDetailsProductId,
-            "10/13/2017" => ProductCalendar::NoDetailsProductId  
-        );
-
+    function __construct($calendarTitle, $productValues) {
+        $this->calendarTitle = $calendarTitle;
+        $this->products = $productValues;
     }
 
     function renderCalendar() {
 
-        HtmlHelpers::writeH2("October 2017");
+        HtmlHelpers::writeH2($this->calendarTitle);
 
         HtmlHelpers::writeTableStartTag("color: #000000; vertical-align: top;");
 
@@ -69,7 +38,7 @@ class ProductCalendar {
                 $columnNumber = 0;
             }
             if ($productId === ProductCalendar::NoLunchProductId) {
-                $dateString = date_format(new DateTime($productDate), "F d, Y");
+                $dateString = date_format(new DateTime($productDate), "M d, Y");
                 HtmlHelpers::writeTableCell("$dateString <p><i>No lunch</i></p>", ThemeConstants::TableCellNothingToOrderStyle);
             } elseif ($productId === ProductCalendar::NoDetailsProductId) {
                 HtmlHelpers::writeTableCell("&nbsp;", ThemeConstants::TableCellNothingToOrderStyle);                
--- PHPfiles/order_page_template.php
+++ PHPfiles/order_page_template.php
@@ -57,8 +57,110 @@ include "ProductCalendar.php";
 	<!-- Menu Table -->
 
 <?php
-  $myCalendar = new ProductCalendar();
-	$myCalendar->renderCalendar();
+  $octoberProducts = array(
+		"10/02/2017" => ProductCalendar::NoLunchProductId,
+		"10/03/2017" => ProductCalendar::NoLunchProductId,
+		"10/04/2017" => ProductCalendar::NoLunchProductId,
+		"10/05/2017" => ProductCalendar::NoLunchProductId,
+		"10/06/2017" => ProductCalendar::NoLunchProductId,
+
+		"10/09/2017" => ProductCalendar::NoLunchProductId,
+		"10/10/2017" => ProductCalendar::NoLunchProductId,
+		"10/11/2017" => ProductCalendar::NoLunchProductId,
+		"10/12/2017" => ProductCalendar::NoLunchProductId,
+		"10/13/2017" => ProductCalendar::NoLunchProductId,
+
+		"10/16/2017" => ProductCalendar::NoLunchProductId,
+		"10/17/2017" => ProductCalendar::NoLunchProductId,
+		"10/18/2017" => ProductCalendar::NoLunchProductId,
+		"10/19/2017" => ProductCalendar::NoLunchProductId,
+		"10/20/2017" => ProductCalendar::NoLunchProductId,
+
+		"10/23/2017" => 401,
+		"10/24/2017" => 401,
+		"10/25/2017" => 401,
+		"10/26/2017" => 401,
+		"10/27/2017" => 401,
+
+		"10/30/2017" => 448,
+		"10/31/2017" => 401,
+		"11/01/2017" => ProductCalendar::NoDetailsProductId,
+		"11/02/2017" => ProductCalendar::NoDetailsProductId,
+		"11/03/2017" => ProductCalendar::NoDetailsProductId,
+);
+
+$novemberProducts = array(
+		"10/30/2017" => ProductCalendar::NoDetailsProductId,
+		"10/31/2017" => ProductCalendar::NoDetailsProductId,
+		"11/01/2017" => 401,
+		"11/02/2017" => 401,
+		"11/03/2017" => 401,
+
+		"11/06/2017" => 401,
+		"11/07/2017" => 401,
+		"11/08/2017" => 401,
+		"11/09/2017" => 401,
+		"11/10/2017" => 401,
+
+		"11/13/2017" => 401,
+		"11/14/2017" => 401,
+		"11/15/2017" => 401,
+		"11/16/2017" => 401,
+		"11/17/2017" => 401,
+
+		"11/20/2017" => 401,
+		"11/21/2017" => 401,
+		"11/22/2017" => 401,
+		"11/23/2017" => ProductCalendar::NoLunchProductId,
+		"11/24/2017" => ProductCalendar::NoLunchProductId,
+
+		"11/27/2017" => 401,
+		"11/28/2017" => 401,
+		"11/29/2017" => 401,
+		"11/30/2017" => 448,
+		"12/01/2017" => ProductCalendar::NoDetailsProductId,
+);
+
+$decemberProducts = array(
+		"11/27/2017" => ProductCalendar::NoDetailsProductId,
+		"11/28/2017" => ProductCalendar::NoDetailsProductId,
+		"11/29/2017" => ProductCalendar::NoDetailsProductId,
+		"11/30/2017" => ProductCalendar::NoDetailsProductId,
+		"12/01/2017" => 448,
+
+		"12/04/2017" => 401,
+		"12/05/2017" => 401,
+		"12/06/2017" => 401,
+		"12/07/2017" => 401,
+		"12/08/2017" => 401,
+
+		"12/11/2017" => 401,
+		"12/12/2017" => 401,
+		"12/13/2017" => 401,
+		"12/14/2017" => 401,
+		"12/15/2017" => 401,
+
+		"12/18/2017" => 401,
+		"12/19/2017" => 401,
+		"12/20/2017" => 401,
+		"12/21/2017" => 401,
+		"12/22/2017" => 401,
+
+		"12/25/2017" => ProductCalendar::NoLunchProductId,
+		"12/26/2017" => ProductCalendar::NoLunchProductId,
+		"12/27/2017" => 401,
+		"12/28/2017" => 401,
+		"12/29/2017" => 401,
+);
+
+$octoberCalendar = new ProductCalendar("October 2017", $octoberProducts);
+$octoberCalendar->renderCalendar();
+
+$novemberCalendar = new ProductCalendar("November 2017", $novemberProducts);
+$novemberCalendar->renderCalendar();
+
+$decemberCalendar = new ProductCalendar("December 2017", $decemberProducts);
+$decemberCalendar->renderCalendar();
 ?>
 	  
       </main>
